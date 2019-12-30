48f4d3443ee588379a840c58beea9317b257ad4b
Corey Hodge <contact@coreyhodge.net>
2019-12-30 02:54:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,7 +52,7 @@ def total_gross(source)
   #    totalGross += directors_totals(source)[list_of_directors[i]]
   #    i += 1
 #  end
-pp list_of_directors(source)[0]
+#pp list_of_directors(source)[0]
 pp directors_totals(source)
 #  while directorCounter < directors_totals(source).length
 #  while directorCounter < source.length do
